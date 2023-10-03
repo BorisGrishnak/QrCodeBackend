1c04aeff8db3c0cedd6be44f17e091e2b239d098
Boris Grishnak <58435887+BorisGrishnak@users.noreply.github.com>
2023-10-03 16:00:10 +0700

    Update

---
+++ QrCode/Controllers/AdminController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QrCode.Models;
+
+namespace QrCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyPolicy")]
+    public class AdminController : ControllerBase
+    {
+        QrCodeContext _context;
+        public AdminController(QrCodeContext qrCodeContext)
+        {
+            _context = qrCodeContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Pengunjung>> GetPengunjung()
+        {
+            var a = _context.Pengunjung.Join(_context.Checkpoints,
+            Pengunjung => Pengunjung.IdPengunjung,
+            Checkpoint => Checkpoint.Pengunjung.IdPengunjung,
+            (Pengunjung, Checkpoint) => new
+            {
+                IdPengunjung = Checkpoint.IdPengunjung,
+                Nama = Pengunjung.Nama,
+                NoHP = Pengunjung.NoHP,
+                Kantor = Pengunjung.Kantor,
+                Expose = Checkpoint.Expose,
+                Force = Checkpoint.Force,
+                BTS = Checkpoint.BTS,
+                UTCall = Checkpoint.UTCall
+            }
+            ).ToList();
+
+            return Ok(a);
+        }
+
+    }
+}
